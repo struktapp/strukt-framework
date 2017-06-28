204dbfe36529fcbc041ba4ab2b991ba896df1a6b
pitsolu <pitsolu@gmail.com>
2017-06-29 02:24:48 +0300

    bootstrap block native repo snippet, composer added phpunit, added travis.yml

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 
-$sibling_dir = array();
-$upper_dir = str_replace(basename(__DIR__), "", __DIR__);
-foreach(scandir("../") as $folder)
-	if(!preg_match("/\./", $folder))
-		$sibling_dir[] = sprintf("%s%s/src", $upper_dir, $folder);
+// $sibling_dir = array();
+// $upper_dir = str_replace(basename(__DIR__), "", __DIR__);
+// foreach(scandir("../") as $folder)
+// 	if(!preg_match("/\./", $folder))
+// 		$sibling_dir[] = sprintf("%s%s/src", $upper_dir, $folder);
 
 $loader = require 'vendor/autoload.php';
-$loader->add('Strukt', $sibling_dir);
+// $loader->add('Strukt', $sibling_dir);
 $loader->add('App', __DIR__.'/lib/');
 $loader->add('Payroll', __DIR__.'/fixtures/root/app/src/');
 
