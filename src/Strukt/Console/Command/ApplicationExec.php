e3631af757da69c4cc40d2208a5c14afa2dad02e
pitsolu <pitsolu@gmail.com>
2023-03-02 11:41:12 +0300

    xcli app server with log option

--- src/Strukt/Console/Command/ApplicationExec.php
+++ src/Strukt/Console/Command/ApplicationExec.php
@@ -11,16 +11,29 @@ use Strukt\Templator as Tpl;
 
 /**
 * app:exec     Run application
+*
+* Usage:
+*
+*      app:exec [<port>] [--log]
+*
+* Arguments:
+*
+*      port  optional: Http port overide 
+*
+* Options:
+*
+*      --log -l   Flag to enable logs
 */
 class ApplicationExec extends \Strukt\Console\Command{
 
 	public function execute(Input $in, Output $out){
 
 		$host = Env::get("serve_host");
-		$port = Env::get("serve_port");
 		$ctx = Env::get("serve_ctx");
 
-		// echo(sprintf("Served@%s:%s", $host, $port));
+		$port = $in->get("port"); //port override
+		if(empty($port))
+			$port = Env::get("serve_port");
 
 		$output = Tpl::create("php -S {{host}}:{{port}} -t {{ctx}}", array(
 
@@ -29,9 +42,27 @@ class ApplicationExec extends \Strukt\Console\Command{
 			"ctx"=>$ctx
 		));
 
-		Process::switchChannels();
+		$inputs = [];
+		if(!is_null($in->getInputs()))
+			$inputs = $in->getInputs();
+
+		$channels_switched = false;
+		if(array_key_exists("log", $inputs)){
+
+			Process::switchChannels();
+			$channels_switched = true;
+		}
+
+		if(!$channels_switched)
+			echo(sprintf("Served@%s:%s", $host, $port));
+		
 		$ps = Process::run([$output], function($streamOutput){
 
+			// $match = "#\[.*\] \d+\.\d+\.\d+\.\d+\:\d+#";
+			// $log = sprintf("%s\n", trim(preg_replace($match, "", $streamOutput)));
+			// if(!in_array($log, ["Accepted", "Closing"]))
+				// echo $log;
+			
 			echo $streamOutput;
 		});
 	}
