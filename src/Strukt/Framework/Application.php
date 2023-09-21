d0ba16079a5786565efdeeaa113f3b55172228d1
pitsolu <pitsolu@gmail.com>
2023-09-21 11:14:24 +0300

    put module routes and facets into name registry|nr

--- src/Strukt/Framework/Application.php
+++ src/Strukt/Framework/Application.php
@@ -2,14 +2,17 @@
 
 namespace Strukt\Framework;
 
+use Strukt\Router\Kernel as RouterKernel;
 use Strukt\Framework\Contract\Module;
 use Strukt\Fs;
 
 class Application{
 
-	public function __construct(){
+	private $router;
 
-		//
+	public function __construct(RouterKernel $router){
+
+		$this->router = $router;
 	}
 
 	public function register(Module $module){
@@ -24,8 +27,9 @@ class Application{
 
 		$base_ns = str($app_name)->concat("\\")->concat($module_name)->yield();
 		$facets = array_flip(config("module.folder*"));
+		$alias = str($alias)->toLower()->yield();
 
-		arr(Fs::lsr($dir))->each(function($k, $v) use($dir, $base_ns, $facets, $alias, $module_class){
+		$route_nss = arr(Fs::lsr($dir))->each(function($k, $v) use($dir, $base_ns, $facets, $alias, $module_class){
 
 			$base_path = trim(str($v)->replace([$dir,".php"], "")->yield(),"/");
 			if(!str($base_path)->equals($module_class)){
@@ -34,7 +38,7 @@ class Application{
 
 				$base_alias = null;
 				if(array_key_exists($facet, $facets))
-					$base_alias = sprintf("%s.%s.%s", strtolower($alias), $facets[$facet], $class);
+					$base_alias = sprintf("%s.%s.%s", $alias, $facets[$facet], $class);
 
 				$facet_ns = str($base_path)
 								->replace("/","\\")
@@ -43,8 +47,18 @@ class Application{
 											->yield())->yield();
 
 				reg(sprintf("nr.%s", $base_alias), $facet_ns);
+
+				if(str($facet)->equals("Router"))
+					return $facet_ns;
+
+				return null;
 			}
 		});
+
+		reg(sprintf("nr.routes.%s", $alias), array_filter($route_nss->yield(), function($v){
+
+			return !is_null($v);
+		}));
 	}
 
 	public function run(){
