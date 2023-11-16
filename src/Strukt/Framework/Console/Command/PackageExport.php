3df787ab19aaa5046fff4c940130f64aa57dc896
pitsolu <pitsolu@gmail.com>
2023-11-16 15:29:35 +0300

    refactor pkg:export

--- src/Strukt/Framework/Console/Command/PackageExport.php
+++ src/Strukt/Framework/Console/Command/PackageExport.php
@@ -4,28 +4,28 @@ namespace Strukt\Framework\Console\Command;
 
 use Strukt\Console\Input;
 use Strukt\Console\Output;
-use Strukt\Fs;
-use Strukt\Type\Str;
-use Strukt\Type\Json;
-use Strukt\Env;
+// use Strukt\Fs;
+// use Strukt\Type\Str;
+// use Strukt\Type\Json;
+// use Strukt\Env;
 
 /**
-* package:export  Export package to another folder
+* package:export  Export package
 * 
 * Usage:
 *	
-*      package:export <as_name>
+*      package:export <name>
 *
 * Arguments:
 *
-*      as_name     Folder to export package
+*      name     New folder name
 */
 class PackageExport extends \Strukt\Console\Command{
 
 	public function execute(Input $in, Output $out){
 
-		$as_name = $in->get("as_name");
-		$pkg_fldr = sprintf("pkg-%s", $as_name);
+		$name = $in->get("name");
+		$pkg_fldr = sprintf("pkg-%s", $name);
 
 		$composer = array(
 
@@ -51,21 +51,39 @@ class PackageExport extends \Strukt\Console\Command{
 		    "minimum-stability"=>"dev"
 		);
 
+		$fsRoot = fs();
+		$fsRoot->mkdir($pkg_fldr);
 
-		Fs::mkdir($pkg_fldr);
-		Fs::mkdir(sprintf("%s/package", $pkg_fldr));
-		Fs::mkdir(sprintf("%s/src", $pkg_fldr));
+		$fsPkg = fs($pkg_fldr);
+		$fsPkg->mkdir("package");
+		$fsPkg->mkdir("src");
 
-		Fs::cpr("package", sprintf("%s/package", $pkg_fldr));
-		Fs::cpr("src", sprintf("%s/src", $pkg_fldr));
-		// copy("composer.json", sprintf("%s/composer.json", $pkg_fldr));
-		copy(".env", sprintf("%s/.env", $pkg_fldr));
-		copy(".gitignore", sprintf("%s/.gitignore", $pkg_fldr));
-		Fs::touchWrite(sprintf("%s/composer.json", $pkg_fldr), Json::pp($composer));
-		Fs::touchWrite(sprintf("%s/README.md", $pkg_fldr), implode("\n", array(
+		$fsRoot->cpr("../package", "package");
+		$fsRoot->cpr("../src", "src");
+		$fsRoot->cpr("../.env", ".env");
+		$fsRoot->cpr("../.gitignore", ".gitignore");
 
-			sprintf("Strukt %s", ucfirst($as_name)),
+		$fsPkg->touchWrite("composer.json", Json::pp($composer));
+		$fsPkg->touchWrite("README.md", implode("\n", array(
+
+			sprintf("Strukt %s", ucfirst($name)),
 			"==="
 		)));
+
+		// Fs::mkdir($pkg_fldr);
+		// Fs::mkdir(sprintf("%s/package", $pkg_fldr));
+		// Fs::mkdir(sprintf("%s/src", $pkg_fldr));
+
+		// Fs::cpr("package", sprintf("%s/package", $pkg_fldr));
+		// Fs::cpr("src", sprintf("%s/src", $pkg_fldr));
+		// copy("composer.json", sprintf("%s/composer.json", $pkg_fldr));
+		// copy(".env", sprintf("%s/.env", $pkg_fldr));
+		// copy(".gitignore", sprintf("%s/.gitignore", $pkg_fldr));
+		// Fs::touchWrite(sprintf("%s/composer.json", $pkg_fldr), Json::pp($composer));
+		// Fs::touchWrite(sprintf("%s/README.md", $pkg_fldr), implode("\n", array(
+
+			// sprintf("Strukt %s", ucfirst($as_name)),
+			// "==="
+		// )));
 	}
 }
