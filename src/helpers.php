89525cd98ead1f9881b5441c2af69db10bb7348c
pitsolu <pitsolu@gmail.com>
2023-11-17 14:02:01 +0300

    phar for env setter

--- src/helpers.php
+++ src/helpers.php
@@ -1,15 +1,32 @@
 <?php
 
+if(!function_exists("phar")){
+
+	env("phar", false);
+	function phar(bool $enable = null){
+
+		if($enable)
+			env("phar", $enable);
+
+		return env("phar");
+	}
+}
+
 if(!function_exists("config")){
 
 	function config(string $key, array|string $options = null){
 
 		if(!reg()->exists("config")){
 
-			$ini_files = glob("cfg/*.ini");
+			$cfg_path = str("cfg/");
+			if(env("phar"))
+				if(Phar::running())
+					$cfg_path = $cfg_path->prepend("phar://");
+
+			$ini_files = glob($cfg_path->concat("*.ini")->yield());
 			foreach($ini_files as $ini_file){
 
-				$facet = str($ini_file)->replace(["cfg/",".ini"],"")->yield();
+				$facet = str($ini_file)->replace([$cfg_path->yield(),".ini"],"")->yield();
 				$configs = parse_ini_file($ini_file);
 				foreach($configs as $name=>$val)
 					reg(sprintf("config.%s.%s", $facet, $name), $val);
