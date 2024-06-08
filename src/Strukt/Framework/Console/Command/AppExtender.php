9cfff9f95e0782db2dcbcdeadd9a4f4ca4b0d7ac
Samuel Weru <pitsolu@gmail.com>
2024-06-08 19:13:06 +0300

    extender refactor

--- src/Strukt/Framework/Console/Command/AppExtender.php
+++ src/Strukt/Framework/Console/Command/AppExtender.php
@@ -13,13 +13,13 @@ class AppExtender extends \Strukt\Console\Command{
 	public function execute(Input $in, Output $out){
 
 		$rel_pkg_ext = env("rel_pkg_ext");
-		if(fs(str("lib/Strukt/Package"))->isFile("Extender.php"))
+		if(fs(str("src/Strukt/Package"))->isFile("Extender.php"))
 			raise("Strukt\\Package\\Extender already exists!");
 		
 		$ext = fs()->cat($rel_pkg_ext);
-		fs("lib")->mkdir("Strukt/Package");
-		fs("lib/Strukt/Package")->touchWrite("Extender.php", $ext);
+		fs()->mkdir("src/Strukt/Package");
+		fs("src/Strukt/Package")->touchWrite("Extender.php", $ext);
 
-		$out->add("Created Extender@lib/Strukt/Package.");
+		$out->add("Created Extender@src/Strukt/Package.");
 	}
 }
